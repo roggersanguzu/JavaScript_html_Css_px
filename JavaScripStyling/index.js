dda54297ea931c5805a8028b945d2a21ab47728e
Roggers Anguzu <141458053+roggersanguzu@users.noreply.github.com>
2024-04-30 19:37:37 +0300

    styling in JavaScript

---
+++ JavaScripStyling/index.js
@@ -0,0 +1,10 @@
+var x = document.querySelector("h1");
+x.style.color = "skyblue";
+x.style.textAlign = "center";
+
+var cont = document.querySelector(".container");
+cont.style.height = "400px";
+cont.style.backgroundColor = "#B51B75";
+
+var y = document.querySelector("p");
+y.style.fontFamily = "sans-serif";
